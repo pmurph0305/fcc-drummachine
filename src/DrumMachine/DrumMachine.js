31d1495a4f446c5f803cbef27102fb995b2ee848
pmurph0305 <pmurph0305@gmail.com>
2019-06-07 10:29:41 -0400

    User story #4

--- src/DrumMachine/DrumMachine.js
+++ src/DrumMachine/DrumMachine.js
@@ -9,15 +9,15 @@ import './DrumMachine.css';
 //  and an inner text that corresponds to one of the following keys on the keyboard: Q, W, E, A, S, D, Z, X, C. 
 //  The drum pads MUST be in this order.
 const DrumKeys = [
-  {key: "Q", audioDesc: "Q"},
-  {key: "W", audioDesc: "W"},
-  {key: "E", audioDesc: "E"},
-  {key: "A", audioDesc: "A"},
-  {key: "S", audioDesc: "S"},
-  {key: "D", audioDesc: "D"},
-  {key: "Z", audioDesc: "Z"},
-  {key: "X", audioDesc: "X"},
-  {key: "C", audioDesc: "C"},
+  {key: "Q", audioDesc: "Q", fileName:"Clap-clap.wav"},
+  {key: "W", audioDesc: "W", fileName:"Quarter-clap.wav"},
+  {key: "E", audioDesc: "E", fileName:"Taxi-clap.wav"},
+  {key: "A", audioDesc: "A", fileName:"Damn-hat.wav"},
+  {key: "S", audioDesc: "S", fileName:"Kraft-hat.wav"},
+  {key: "D", audioDesc: "D", fileName:"Superb-hat.wav"},
+  {key: "Z", audioDesc: "Z", fileName:"Dynamite-snare.wav"},
+  {key: "X", audioDesc: "X", fileName:"Lame-snare.wav"},
+  {key: "C", audioDesc: "C", fileName:"Hashbrown-kick.wav"},
 ]
 
 class DrumMachine extends React.Component {
@@ -29,7 +29,7 @@ class DrumMachine extends React.Component {
       <div className="drum-machine" id="drum-machine">
         <div className="drum-key-container">
           <div className="drum-key-grid">
-            {DrumKeys.map(drum => <DrumPad keyVal={drum.key} audioDesc={drum.audioDesc}/>)}
+            {DrumKeys.map(drum => <DrumPad key={drum.key} keyVal={drum.key} audioDesc={drum.audioDesc} fileName={drum.fileName}/>)}
           </div>
         </div>
         {/* User Story #2: Within #drum-machine I can see an element with a corresponding id="display". */}
--- src/DrumPad/DrumPad.js
+++ src/DrumPad/DrumPad.js
@@ -3,9 +3,13 @@ import './DrumPad.css';
 
 
 
-const DrumPad = ({keyVal, audioDesc}) => {
+const DrumPad = ({keyVal, audioDesc, fileName}) => {
   return (
-    <button className="btn btn-primary drum-pad" id={audioDesc}>{keyVal}</button>
+    <button className="btn btn-primary drum-pad" id={audioDesc}>
+    {keyVal}
+    {/*  Within each .drum-pad, there should be an HTML5 <audio> element which has a src attribute pointing to an audio clip, a class name of "clip", and an id corresponding to the inner text of its parent .drum-pad (e.g. id="Q", id="W", id="E" etc.). */}
+      <audio className="clip" id={keyVal}  src={process.env.PUBLIC_URL +"/DrumSamples/"+fileName} type="audio/wav"/>
+    </button>
   )
 }
 
